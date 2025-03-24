28368f2ba18731bffb3c5c86afbe92dd6132e780
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-03-24 09:16:53 -0500

    update

---
+++ test.py
@@ -0,0 +1,5 @@
+from utils._classification._tars_based import *
+
+classes = ['race stereotype', 'racial bias']
+
+tars_classifier_zero(list_sentences:List[str]|str, classes:List[str]|str, model:str='tars-base')
