0944693155f0bbf8f3c6022e828774fecaa01aa5
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-04-29 15:56:28 -0500

    update run identification

--- run_identification.py
+++ run_identification.py
@@ -7,20 +7,6 @@ def main(path_data:str,
     mpterm = MPTerm(path_data=path_data,
                     dir_output=dir_output, file_output=file_output)
 
-    # Check if path_data exists
-    # If not exist throw error
-
-    # Create dir_output folder
-
-    # procmine = ProcMine(path_data=path_data, path_map=path_map,
-    #                     dir_output=dir_output, file_output=file_output)
-    
-    # # Load data file, load map file, check output directory, check entities directory
-    # procmine.prepare_data_paths()
-
-    # # Process database
-    # procmine.process()
-
     # Save output
     mpterm.save_output()
 
@@ -30,14 +16,11 @@ if __name__ == '__main__':
     parser.add_argument('--path_data', required=True,
                         help='Directory or file where the mineral site database is located')
     
-    # parser.add_argument('--path_map',
-    #                     help='CSV file with label mapping information.')
-    
-    parser.add_argument('--output_directory', default='./output',
-                        help='Directory for processed mineral site database')
+    parser.add_argument('--output_dir', default='./output',
+                        help='Directory of output')
     
     parser.add_argument('--output_filename', required=True
-                        help='Filename for processed mineral site database')
+                        help='Filename of output')
     
     args = parser.parse_args()
 
