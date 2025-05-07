2929f7829a805107ffd4ccb69c6a4bb2c63d4c9e
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-05-07 16:09:36 -0500

    pipeline complete; testing needed

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # (In Progress) Mapping Prejudice: Racial Term Identification from Historical Deed Documents
 
+## TODO: COMPONENTS TO CHECK
+- [ ] class init with local data
+    - [ ] information dictionary without UUID
+- [ ] class init with json input
+    - [ ] information dictionary conversation from str->json
+    - [ ] load direct with json (not through argparse)
+- [ ] entity recognizer running
+    - [ ] sentence: list ners
+    - [ ] ner: list numbers + default information
+- [ ] saving to local or
+- [ ] json output print out
+
+
+- [ ] Remove unnecessary functions
+- [ ] Fill comment
+
 ## Installation:
 To Be Filled
 
 ## Usage:
-To Be Filled
-
-#### TODO:
-- [ ] Refer line 79 on mpterm.py (Converting for identification)
+When implementing directly in S3 pipeline:
+```
+use main in run_identification with
+main(json_input: JSON from )
+```
--- run_identification.py
+++ run_identification.py
@@ -1,26 +1,41 @@
 import argparse
+from typing import Union
 from mpterm import MPTerm
 
-def main(path_data:str,
-         dir_output:str,file_output:str) -> None:
+def main(path_data:str=None, json_input:Union[str, dict]=None,
+         dir_output:str=None, file_output:str=None,
+         bool_local:bool=False):
     
-    mpterm = MPTerm(path_data=path_data,
-                    dir_output=dir_output, file_output=file_output)
+    if bool_local:
+        mpterm = MPTerm(input_info=path_data,
+                        dir_output=dir_output, file_output=file_output)
+    else:
+        mpterm = MPTerm(input_info=json_input,
+                        dir_output=dir_output, file_output=file_output)
     
     # Load data to variables
-    mpterm.load_data()
+    mpterm.load_data(bool_local=bool_local)
 
     # Run entity recogntiion
     mpterm.entity_recog()
 
-    # Save output
-    mpterm.save_output()
+    if bool_local:
+        # Save output
+        mpterm.save_output()
+    else:
+        return mpterm.json_output
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Mapping Prejudice')
 
-    parser.add_argument('--path_data', required=True,
+    parser.add_argument('--local', default=False,
+                        help='Indication if running on local', action='store_true')
+
+    parser.add_argument('--path_data', default=None,
                         help='Path to the deed document')
+    
+    parser.add_argument('--json_input', default=None,
+                        help='String format JSON input')
     
     parser.add_argument('--output_dir', default='./output',
                         help='Directory of output')
@@ -30,5 +45,6 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    main(path_data=args.path_data,
-         dir_output=args.output_dir, file_output=args.output_file)
+    main(path_data=args.path_data, json_input=args.json_input,
+         dir_output=args.output_dir, file_output=args.output_file,
+         bool_local=args.local)
