befa454dc9c0a7ab13558ca2f5fa670ce41b6d10
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-05-19 10:10:17 -0500

    pip version checked

--- run_classification.py
+++ run_classification.py
@@ -6,27 +6,12 @@ def main(path_data:str=None, json_input:Union[str, dict]=None,
          dir_output:str=None, file_output:str=None,
          bool_local:bool=False):
     
-    # if bool_local:
-    #     mpterm = MPTerm(input_info=path_data,
-    #                     dir_output=dir_output, file_output=file_output)
-    # else:
-    #     mpterm = MPTerm(input_info=json_input,
-    #                     dir_output=dir_output, file_output=file_output)
-
-    dummy_json = {
-        "statusCode": 200,
-        "body": {
-            "message": "hello world",
-            "bucket": "covenants-deed-images",
-            "orig_img": "raw/test-county/Deeds/19499142059800.001.tif",
-            "ocr_json": "ocr/json/mn-anoka-county/9/30677860.json",
-            "web_img": "web/test-county/Deeds/ceb01a595e884949a3d06edd6ccdfffc.jpg",
-            "uuid": "ceb01a595e884949a3d06edd6ccdfffc"
-        }
-    }
-
-    mpterm = MPTerm(input_info=dummy_json,
-                    dir_output=dir_output, file_output=file_output)
+    if bool_local:
+        mpterm = MPTerm(input_info=path_data,
+                        dir_output=dir_output, file_output=file_output)
+    else:
+        mpterm = MPTerm(input_info=json_input,
+                        dir_output=dir_output, file_output=file_output)
 
     # Load data to variables
     mpterm.load_data(bool_local=bool_local)
